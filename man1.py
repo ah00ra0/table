b9f915c22c4d55f80e389843be21225cc3fdb42e
ahoora <wahooraw@gmail.com>
2025-01-11 19:47:39 +0000

    Add files via upload

---
+++ man1.py
@@ -0,0 +1,30 @@
+from colorama import Style,Fore
+print(Fore.LIGHTGREEN_EX+r'''
+    
+    
+          ________________________
+        | ahoora hacking hello !!  |
+          ========================
+                                \
+                                \
+                                ^__^
+                                (oo)\_______
+                                (__)\       )\/\
+                                    ||----w |
+                                    ||     ||
+        ┏━━━━━━━━━┳━━━━━━━━━━━━━━━━┓
+        ┃ Options ┃ Utility        ┃
+        ┡━━━━━━━━━╇━━━━━━━━━━━━━━━━┩
+        │ 1       │ file maker     │
+        │ 2       │ file copy      |
+        | 3       | send telgram   │
+        │ 4       │ Subdomain Enum │
+        │ 5       │ GEO-IP Enum    │
+        | 6       | JWT Analysis   |
+        | 7       | PCAP Analysis  |
+        | 8       | Hash Cracker   |
+        │ q       │ Quit           │
+        └─────────┴────────────────┘
+        Enter your choice:
+      سلام من اهورا هستم هکر و برنامه نویس وب !!!
+      '''+Style.RESET_ALL)
